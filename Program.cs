8dd31a3b72f86df5855a40793ee17f68dece0b61
Sreekumar <sreekumar@thinksys.com>
2024-07-26 15:42:32 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+using IPassM.Components;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace IPassM
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+                    options.SlidingExpiration = true;
+                    options.AccessDeniedPath = "/Forbidden/";
+                }); ;
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCookiePolicy(new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = SameSiteMode.Strict,
+            });
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
